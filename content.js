735a658082cb5e04ce21eb13669c6220d2afd82e
chasingboy <409562143@qq.com>
2024-07-17 11:39:48 +0800

    reformat

--- content.js
+++ content.js
@@ -14,27 +14,27 @@ chrome.runtime.onMessage.addListener((message) => {
 });
 
 function showMask() {
-    if (!mask) {
-      mask = document.createElement('div');
-      mask.style.position = 'fixed';
-      mask.style.bottom = '50px'; // 设置距离底部 50px
-      mask.style.left = '50%'; // 水平居中
-      mask.style.transform = 'translateX(-50%)'; // 水平居中
-      mask.style.width = '800px';
-      mask.style.height = '50px';
-      mask.style.backgroundColor = 'black';
-      mask.style.zIndex = '10000';
-      mask.style.cursor = 'move';
-      mask.style.resize = 'both';
-      mask.style.overflow = 'hidden';
-      document.body.appendChild(mask);
-  
-      mask.addEventListener('mousedown', onMouseDown);
-      mask.addEventListener('mouseup', onMouseUp);
-      mask.addEventListener('mousemove', onMouseMove);
-      mask.addEventListener('click', preventClick);
-    }
+  if (!mask) {
+    mask = document.createElement('div');
+    mask.style.position = 'fixed';
+    mask.style.bottom = '50px'; // 设置距离底部 50px
+    mask.style.left = '50%'; // 水平居中
+    mask.style.transform = 'translateX(-50%)'; // 水平居中
+    mask.style.width = '800px';
+    mask.style.height = '50px';
+    mask.style.backgroundColor = 'black';
+    mask.style.zIndex = '10000';
+    mask.style.cursor = 'move';
+    mask.style.resize = 'both';
+    mask.style.overflow = 'hidden';
+    document.body.appendChild(mask);
+
+    mask.addEventListener('mousedown', onMouseDown);
+    mask.addEventListener('mouseup', onMouseUp);
+    mask.addEventListener('mousemove', onMouseMove);
+    mask.addEventListener('click', preventClick);
   }
+}
 
 function hideMask() {
   if (mask) {
